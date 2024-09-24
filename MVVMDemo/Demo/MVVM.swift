e8e7569cac7ebca8ed86ddd81f0bf633b7148d9e
VimalKer <kervimal942kk@gmail.com>
2024-09-24 13:42:13 +0530

    new MVVM demo

--- MVVMDemo/Demo/MVVM.swift
+++ MVVMDemo/Demo/MVVM.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class Model{
+    
+    var strName:String!
+    var strEmail:String!
+ 
+    static var sharedInstance  =  Model()
+    
+    func getAPI<T : Decodable>(demo:T,method:T,header:T,parameter:T,url:T){
+        
+    }
+}
+
+enum products:String{
+    case protocols
+    case system
+}
